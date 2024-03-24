f453849b73bad5191aba28b9a1c6bfecfe631945
James Conlon <james.conlon@fr.fcm.travel>
2024-03-24 17:15:28 +0100

    register form

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -4,7 +4,7 @@ import { useState, useTransition } from 'react';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { LoginSchema } from '@/schemas';
+import { RegisterSchema } from '@/schemas';
 import { CardWrapper } from './card-wrapper';
 import {
 	Form,
@@ -18,27 +18,28 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { FormError } from '@/components/form-error';
 import { FormSuccess } from '@/components/form-success';
-import { login } from '@/../actions/login';
+import { register } from '@/../actions/register';
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
 	const [isPending, startTransition] = useTransition();
 	const [error, setError] = useState<string | undefined>('');
 	const [success, setSuccess] = useState<string | undefined>('');
 
-	const form = useForm<z.infer<typeof LoginSchema>>({
-		resolver: zodResolver(LoginSchema),
+	const form = useForm<z.infer<typeof RegisterSchema>>({
+		resolver: zodResolver(RegisterSchema),
 		defaultValues: {
 			email: '',
 			password: '',
+			name: '',
 		},
 	});
 
-	const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+	const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
 		setError(undefined);
 		setSuccess(undefined);
 
 		startTransition(() => {
-			login(values).then((data) => {
+			register(values).then((data) => {
 				setError(data.error);
 				setSuccess(data.success);
 			});
@@ -47,9 +48,9 @@ export const LoginForm = () => {
 
 	return (
 		<CardWrapper
-			headerLabel="Welcome back"
-			backButtonLabel="Don't have an account?"
-			backButtonHref="/auth/register"
+			headerLabel="Create an account"
+			backButtonLabel="Already have an account?"
+			backButtonHref="/auth/login"
 			showSocial
 		>
 			<Form {...form}>
@@ -58,6 +59,23 @@ export const LoginForm = () => {
 					className="space-y-6"
 				>
 					<div className="space-y-4">
+						<FormField
+							control={form.control}
+							name="name"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Name</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											disabled={isPending}
+											placeholder="John Doe"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
 						<FormField
 							control={form.control}
 							name="email"
@@ -102,7 +120,7 @@ export const LoginForm = () => {
 						type="submit"
 						className="w-full"
 					>
-						Login
+						Register
 					</Button>
 				</form>
 			</Form>
