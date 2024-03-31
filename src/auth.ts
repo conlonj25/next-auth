8cdf5235f6eafc8750cac932cfa0c425c6bb380e
James Conlon <james.conlon@es.fcm.travel>
2024-03-31 19:24:44 +0200

    callbacks

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,21 @@
-import NextAuth from 'next-auth';
+import NextAuth, { DefaultSession } from 'next-auth';
 
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from '@/lib/db';
 
 import authConfig from '@/auth.config';
+import { getUserById } from '../data/user';
+import { UserRole } from '@prisma/client';
+
+type ExtendedUser = DefaultSession['user'] & {
+	role: 'ADMIN' | 'USER';
+};
+
+declare module 'next-auth' {
+	interface Session {
+		user: ExtendedUser;
+	}
+}
 
 export const {
 	handlers: { GET, POST },
@@ -11,6 +23,28 @@ export const {
 	signIn,
 	signOut,
 } = NextAuth({
+	callbacks: {
+		async session({ token, session }) {
+			if (token.sub && session.user) {
+				session.user.id = token.sub;
+			}
+
+			if (token.role && session.user) {
+				session.user.role = token.role as UserRole;
+			}
+
+			return session;
+		},
+		async jwt({ token }) {
+			if (!token.sub) return token;
+
+			const existingUser = await getUserById(token.sub);
+			if (!existingUser) return token;
+
+			token.role = existingUser.role;
+			return token;
+		},
+	},
 	adapter: PrismaAdapter(db),
 	session: { strategy: 'jwt' },
 	...authConfig,
